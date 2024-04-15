c551a5133d89a6aa131d1789b55e808e83d3a498
nirbhay266 <153939270+nirbhay266@users.noreply.github.com>
2024-04-15 09:15:19 +0530

    added

--- p5.java
+++ p5.java
@@ -1,7 +1,25 @@
+import java.io.IOException;
 import java.util.*;
 public class p5 {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int a[]=new int[5];
+        int b[] = new int[5];
+        int c[] = new int[10];
+        int p=5;
+        for(int i=0; i<5; i++)
+        {
+            System.out.print("Enter the first elements of the array"+(i+1)+":");
+            a[i]=sc.nextInt();
+            System.out.print("Enter the second elements of the array"+(i+1)+":");
+            b[i]=sc.nextInt();
+            c[i]=a[i];
+            c[i+p]=b[i];    
+        }
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(c[i]);
+        }
+        sc.close();
     }
 }
